6f61fd154820a28c44962bfb528b737933045f15
jbkwizera <jeanbaptistekwi@gmail.com>
2022-09-15 17:18:24 +0200

    Scale y-axis to 0-100%

--- R/geom_vbar_dodge.R
+++ R/geom_vbar_dodge.R
@@ -4,7 +4,6 @@
 #' @param var_main The main variable to go on the x-axis
 #' @param var_fill The fill variable to go on the y-axis in the bars
 #' @param wt The weights for tallying
-#' @param percent Plot the percentages
 #' @param title The title of the chart
 #' @param xlab The label of the x-axis
 #' @param ylab The label of the y-axis
@@ -17,7 +16,7 @@
 #' library(ggplot2)
 #' geom_vbar_dodge(mpg, "class", "drv")
 #' @include utils-pipe.R assets.R utils-data.R
-geom_vbar_dodge <- function(data, var_main, var_fill, wt = NULL, percent = FALSE, title = NULL, xlab = NULL, ylab = NULL, caption = NULL) {
+geom_vbar_dodge <- function(data, var_main, var_fill, wt = NULL, title = NULL, xlab = NULL, ylab = NULL, caption = NULL) {
   if (is.null(wt)) {
     data$wt <- 1
   } else {
@@ -31,13 +30,13 @@ geom_vbar_dodge <- function(data, var_main, var_fill, wt = NULL, percent = FALSE
 
   (data %>%
       ggplot2::ggplot(
-        aes(.data[[var_main]], n, fill = .data[[var_fill]], group = .data[[var_fill]])) +
+        aes(.data[[var_main]], percent, fill = .data[[var_fill]], group = .data[[var_fill]])) +
       ggplot2::geom_bar(stat = "identity", position = "dodge") +
       ggplot2::geom_text(
         aes(label = paste0(round(percent), "%")),
         position = ggplot2::position_dodge(0.9), vjust = -0.5, size = 3) +
       ggplot2::scale_fill_manual(
-        values = choose_shades(MPALETTE, length(unique(data[[var_fill]]))),
+        values = COLOR_SET[[length(unique(data[[var_fill]]))]],
         aesthetics = c("color", "fill")) +
       ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0, 0.1))) +
       ggplot2::ggtitle(title) +
@@ -48,8 +47,7 @@ geom_vbar_dodge <- function(data, var_main, var_fill, wt = NULL, percent = FALSE
       ggplot2::theme(
         plot.title = ggplot2::element_text(hjust = 0.5),
         plot.caption = ggplot2::element_text(hjust = 0.5),
-        axis.line.y.left = ggplot2::element_line(color = "lightgray", size = 0.2),
-        axis.ticks.y.left = ggplot2::element_line(color = "lightgray", size = 0.2),
+        axis.text.y = ggplot2::element_blank(),
         legend.margin = ggplot2::margin(2, 2, 2, 2),
         legend.text = ggplot2::element_text(size = 8, vjust = 0.5),
         legend.key.size = ggplot2::unit(8, "pt"),
